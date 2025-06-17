0995c21e9a23d487a5318ba54a147d6c22bbda3f
Anojk123 <140962120+Anojk123@users.noreply.github.com>
2025-06-17 18:03:51 +0530

    17Bootstrap

--- App.jsx
+++ App.jsx
@@ -1,11 +1,24 @@
 
-import KgButton from "./KgButton"
+import './App.css'
+
 function App() {
-  return <div>
-    <h1>This is the best course for react</h1>
-    <KgButton/>
-  </div>
  
+
+  return (
+    <>
+      this is my react app in which add bootstrap.<br/><br/><br/> 
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
+    </>
+  )
 }
 
 export default App
